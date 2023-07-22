b430f6801995f39a862ed52a42778bad523c75cd
404-nut-pound <souls4523@gmail.com>
2023-07-22 18:35:32 +0900

    initial commit

---
+++ src/main/java/io/hskim/learnspringcloudgateway/LearnSpringCloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package io.hskim.learnspringcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringCloudGatewayApplication.class, args);
+  }
+}
---
+++ src/test/java/io/hskim/learnspringcloudgateway/LearnSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package io.hskim.learnspringcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
