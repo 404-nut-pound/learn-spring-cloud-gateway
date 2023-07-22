e7137ff0b48ded91f229f8b61ae6bafacec36f22
404-nut-pound <souls4523@gmail.com>
2023-07-22 22:18:39 +0900

    [feat] Gateway Route를 Configuration으로 설정
    - 커스텀 헤더 추가

---
+++ src/main/java/io/hskim/learnspringcloudgateway/config/FilterConfig.java
@@ -0,0 +1,37 @@
+package io.hskim.learnspringcloudgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+  @Bean
+  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    return builder
+      .routes()
+      .route(r ->
+        r
+          .path("/first-service/**")
+          .filters(f ->
+            f
+              .addRequestHeader("gateway-request", "first-service-request")
+              .addResponseHeader("gateway-response", "first-service-response")
+          )
+          .uri("http://localhost:8081")
+      )
+      .route(r ->
+        r
+          .path("/second-service/**")
+          .filters(f ->
+            f
+              .addRequestHeader("gateway-request", "second-service-request")
+              .addResponseHeader("gateway-response", "second-service-response")
+          )
+          .uri("http://localhost:8082")
+      )
+      .build();
+  }
+}
