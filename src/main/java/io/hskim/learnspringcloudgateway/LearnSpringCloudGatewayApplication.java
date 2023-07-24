7111c9f65ff2468b8ab0495dfd8e4f89df3eb365
404-nut-pound <souls4523@gmail.com>
2023-07-24 19:32:09 +0900

    [feat] Spring Cloud Config 및 Actuator 추가

--- src/main/java/io/hskim/learnspringcloudgateway/LearnSpringCloudGatewayApplication.java
+++ src/main/java/io/hskim/learnspringcloudgateway/LearnSpringCloudGatewayApplication.java
@@ -1,12 +1,20 @@
 package io.hskim.learnspringcloudgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearnSpringCloudGatewayApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LearnSpringCloudGatewayApplication.class, args);
+  }
+
+  @Bean
+  public HttpExchangeRepository httpExchange() {
+    return new InMemoryHttpExchangeRepository();
   }
 }
